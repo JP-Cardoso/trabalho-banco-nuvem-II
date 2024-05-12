069f0c5a1f6edb46dd2107ec5066999ff736236b
jp-cardoso <jhonpoal@hotmail.com>
2024-05-12 19:36:40 -0300

    add: criação do database

--- src/config/database/write-database.js
+++ src/config/database/write-database.js
@@ -1,1 +1,15 @@
 // faz a comunicação com o banco de escrita
+const data = [];
+
+export default class WriteData {
+
+  static insert(item) {
+    data.push(item);
+    return item;
+  }
+
+  static read(index) {
+
+  }
+
+}
