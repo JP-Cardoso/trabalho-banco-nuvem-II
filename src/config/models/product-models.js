48ba27c3ed8944a0cf9af0074e52738f01380b51
jp-cardoso <jhonpoal@hotmail.com>
2024-05-13 12:51:23 -0300

    add: criação do modelo da tabela de produtos

---
+++ src/config/models/product-models.js
@@ -0,0 +1,33 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { writeDb } from "../database/write-database";
+
+const db = writeDb();
+
+export const Product = db.define(
+  'product',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    }, 
+    descricao: {
+      type: DataTypes.STRING(50),
+      allownull: false
+    },
+    categoria: {
+      type: DataTypes.STRING(10),
+      allownull: false
+    },
+    valor: {
+      type: DataTypes.DECIMAL(15, 2)
+    },
+    criado_em: {
+      type: DataTypes.DATE
+    },
+    criado_por: {
+      type: DataTypes.STRING(50),
+      allownull: false
+    }
+  }
+)
